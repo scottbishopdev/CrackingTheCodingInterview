0d9f4a825b7177eaa0ab695818a415e6983b79c1
Scott Bishop <sbishop411@gmail.com>
2021-06-30 23:13:41 -0600

    Implemented working DFS and BFS for 4.1

--- src/com/CtCI/CustomLibrary/DirectedGraphNode.java
+++ src/com/CtCI/CustomLibrary/DirectedGraphNode.java
@@ -7,17 +7,17 @@ public class DirectedGraphNode {
     public String data;
     public ArrayList<DirectedGraphNode> children;
 
-    public DirectedGraphNode(String d) {
-        data = d;
+    public DirectedGraphNode(String value) {
+        data = value;
         children = new ArrayList<DirectedGraphNode>();
     }
 
-    public DirectedGraphNode(String d, DirectedGraphNode[] nodeChildren) {
-        this(d, new ArrayList<DirectedGraphNode>(Arrays.asList(nodeChildren)));
+    public DirectedGraphNode(String value, DirectedGraphNode[] nodeChildren) {
+        this(value, new ArrayList<DirectedGraphNode>(Arrays.asList(nodeChildren)));
     }
 
-    public DirectedGraphNode(String d, ArrayList<DirectedGraphNode> nodeChildren) {
-        data = d;
+    public DirectedGraphNode(String value, ArrayList<DirectedGraphNode> nodeChildren) {
+        data = value;
         children = nodeChildren;
     }
 }
